2c179f7600400feec0e28c390aa483d9db9203fb
kallebysantos <kalleby_santos@hotmail.com>
2025-01-08 20:32:57 +0000

    feat: solve 2024_01

---
+++ 2024/01/main.exs
@@ -0,0 +1,30 @@
+defmodule Mod do
+  def get_total_distance(filename) do
+    numbers =
+      File.read(filename)
+      |> get_number_stream()
+
+    left_part =
+      Stream.take_every(numbers, 2)
+      |> Enum.sort()
+
+    right_part =
+      Stream.drop_every(numbers, 2)
+      |> Enum.sort()
+
+    Stream.zip_with(left_part, right_part, fn a, b -> abs(a - b) end)
+    |> Enum.sum()
+  end
+
+  defp get_number_stream({:ok, file_contents}) do
+    String.split(file_contents)
+    |> Stream.map(&String.to_integer/1)
+  end
+
+  defp get_number_stream({:error, reason}) do
+    raise("Could not load file reason: #{reason}")
+  end
+end
+
+Mod.get_total_distance("./input.txt")
+|> IO.puts()
